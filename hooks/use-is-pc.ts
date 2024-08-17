beaa0e423a9df56939ae54b2fcea34331e351256
mengjian <mengjian@bytedance.com>
2024-08-18 01:43:06 +0800

    chore: Update use-is-pc hook to handle undefined window object

--- hooks/use-is-pc.ts
+++ hooks/use-is-pc.ts
@@ -1,11 +1,18 @@
 import { useState, useEffect } from "react";
 
+const checkIsPC = () => {
+  if (typeof window !== "undefined") {
+    return window.innerWidth >= 640;
+  }
+  return false;
+};
+
 // 自定义 Hook 用于判断是否为 PC 端
 function useIsPC() {
-  const [isPC, setIsPC] = useState(window?.innerWidth >= 640);
+  const [isPC, setIsPC] = useState(checkIsPC());
 
   useEffect(() => {
-    const handleResize = () => setIsPC(window.innerWidth >= 640);
+    const handleResize = () => setIsPC(checkIsPC());
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
