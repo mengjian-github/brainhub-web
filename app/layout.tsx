3d74f3a129dfaceddebcb63e7115c4399611cd72
mengjian <mengjian@bytedance.com>
2024-08-18 02:32:23 +0800

    chore: Update viewport meta tag in layout component to disable user scaling

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,10 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no"
-      />
+      <head>
+        <meta
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          name="viewport"
+        />
+      </head>
       <body>
         {children}
         <Analytics />
